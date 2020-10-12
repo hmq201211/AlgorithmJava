cd51d5093edcf89cc5f6ecb6181e842a4eac538c
hmq201211 <879602728@qq.com>
2020-10-12 15:32:46 +0800

    10/12

--- code/src/main/java/offer032_3/Solution.java
+++ code/src/main/java/offer032_3/Solution.java
@@ -7,39 +7,25 @@ import java.util.*;
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root == null)
-            return null;
-        boolean fromLeft = false;
-        List<List<Integer>> toReturn = new ArrayList<>();
-        Deque<TreeNode> deque = new LinkedList<>();
-        deque.addLast(root);
-        Queue<Deque<TreeNode>> queue = new LinkedList<>();
-        queue.add(deque);
-        while (!queue.isEmpty()) {
-            Deque<TreeNode> treeNodes = queue.poll();
-            List<Integer> temp = new ArrayList<>();
-            Deque<TreeNode> next = new LinkedList<>();
-            while (!treeNodes.isEmpty()) {
-                TreeNode treeNode = treeNodes.pollFirst();
+            return new LinkedList<>();
+        List<List<Integer>> toReturn = new LinkedList<>();
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            LinkedList<Integer> temp = new LinkedList<>();
+            int size = toReturn.size();
+            boolean fromLeft = size % 2 == 0;
+            for (int i = nodes.size(); i > 0; i--) {
+                TreeNode node = nodes.poll();
                 if (fromLeft) {
-                    if (treeNode.right != null) {
-                        next.addFirst(treeNode.right);
-                    }
-                    if (treeNode.left != null) {
-                        next.addFirst(treeNode.left);
-                    }
+                    temp.addLast(node.val);
                 } else {
-                    if (treeNode.right != null) {
-                        next.addLast(treeNode.right);
-                    }
-                    if (treeNode.left != null) {
-                        next.addLast(treeNode.left);
-                    }
+                    temp.addFirst(node.val);
                 }
-                fromLeft = !fromLeft;
+                if (node.left != null) nodes.add(node.left);
+                if (node.right != null) nodes.add(node.right);
             }
             toReturn.add(temp);
-            queue.add(next);
-
         }
         return toReturn;
     }
