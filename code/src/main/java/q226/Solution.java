5998abf8c0e645c0a78f43a6c2bb273f2d54e1d5
hmq201211 <879602728@qq.com>
2020-12-30 23:57:13 +0800

    20201230

---
+++ code/src/main/java/q226/Solution.java
@@ -0,0 +1,26 @@
+package q226;
+
+import common.TreeNode;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree2(root.left);
+        invertTree2(root.right);
+        return root;
+    }
+}
