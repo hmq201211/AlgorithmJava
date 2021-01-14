5a3d43424336085aa192f613b1a1822d00d73d76
hmq201211 <879602728@qq.com>
2021-01-14 17:53:46 +0800

    1-14

---
+++ code/src/main/java/q110/Solution.java
@@ -0,0 +1,22 @@
+package q110;
+
+import common.TreeNode;
+
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1
+                    && isBalanced(root.left)
+                    && isBalanced(root.right);
+        }
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+}
---
+++ code/src/main/java/q110/Solution2.java
@@ -0,0 +1,24 @@
+package q110;
+
+import common.TreeNode;
+
+public class Solution2 {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) >= 0;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (left == -1
+                || right == -1
+                || Math.abs(left - right) > 1) {
+            return -1;
+        } else {
+            return Math.max(left, right) + 1;
+        }
+    }
+}
